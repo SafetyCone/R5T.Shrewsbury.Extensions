ffc469af82a0147a0a52112d6c7b141ded9bcf9d
MinexAutomation <minex.automation@gmail.com>
2019-12-11 16:26:39 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Shrewsbury.Extensions.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Shrewsbury.Extensions.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
